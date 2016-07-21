0f24b761e9006e5c6f14f04376724e7d38762b7c
supriya <pandeydeepali0@gmail.com>
2016-07-21 08:12:59 +0530

    Updates in FoodyApp.

--- app/src/main/java/com/croma/app/foodApp/InfoActivity.java
+++ app/src/main/java/com/croma/app/foodApp/InfoActivity.java
@@ -2,12 +2,44 @@ package com.croma.app.foodApp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
-public class InfoActivity extends AppCompatActivity {
-
+public class InfoActivity extends AppCompatActivity implements GlobalInterFace, View.OnClickListener {
+    private ListView infoLView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        findViewById();
+        setOnClickListener();
+    }
+
+    @Override
+    public void findViewById() {
+        infoLView=(ListView)findViewById(R.id.infoList);
+
+
+
+    }
+
+    @Override
+    public void setOnClickListener() {
+
+    }
+
+    @Override
+    public void applyFont() {
+
+    }
+
+    @Override
+    public void OnitemSelect() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
