a42d6b8c611375a5e75979845b1e293c536e9719
supriya <pandeydeepali0@gmail.com>
2016-07-21 08:37:17 +0530

    Updates in FoodyApp.

--- app/src/main/java/com/croma/app/foodApp/InfoActivity.java
+++ app/src/main/java/com/croma/app/foodApp/InfoActivity.java
@@ -3,21 +3,39 @@ package com.croma.app.foodApp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class InfoActivity extends AppCompatActivity implements GlobalInterFace, View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class InfoActivity extends AppCompatActivity implements GlobalInterFace, AdapterView.OnItemClickListener {
     private ListView infoLView;
+    List<InfoItem> infoItems;
+
+    public static final String[] titles = new String[] { "Partner with us",
+            "Terms and Conditions", "Privacy Policy", "How It Works" };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
         findViewById();
         setOnClickListener();
+        infoItems=new ArrayList<InfoItem>();
+        for (int i = 0; i < titles.length; i++) {
+            InfoItem item = new InfoItem( titles[i]);
+            infoItems.add(item);
+        }
     }
 
     @Override
     public void findViewById() {
         infoLView=(ListView)findViewById(R.id.infoList);
+        InfoAdapter adapter = new InfoAdapter(this, infoItems);
+        infoLView.setAdapter(adapter);
+        infoLView.setOnItemClickListener(this);
+
 
 
 
@@ -38,8 +56,9 @@ public class InfoActivity extends AppCompatActivity implements GlobalInterFace, 
 
     }
 
+
     @Override
-    public void onClick(View v) {
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
     }
 }
