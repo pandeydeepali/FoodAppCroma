00a4d098d706459388943cca10f85d5d770b8269
supriya <pandeydeepali0@gmail.com>
2016-07-24 22:51:38 +0530

    Updates in FoodyApp.

--- app/src/main/java/com/croma/app/foodApp/ListDetailActivityFragment.java
+++ app/src/main/java/com/croma/app/foodApp/ListDetailActivityFragment.java
@@ -1,14 +1,12 @@
 package com.croma.app.foodApp;
-
-import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 import java.util.ArrayList;
 
