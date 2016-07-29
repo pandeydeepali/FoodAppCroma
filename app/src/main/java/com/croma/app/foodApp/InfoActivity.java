2aa3a991af673c7f15cc47ff90a4c0957cf0af0b
supriya <pandeydeepali0@gmail.com>
2016-07-29 22:11:24 +0530

    Updates in FoodyApp.

--- app/src/main/java/com/croma/app/foodApp/InfoActivity.java
+++ app/src/main/java/com/croma/app/foodApp/InfoActivity.java
@@ -1,17 +1,20 @@
 package com.croma.app.foodApp;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class InfoActivity extends AppCompatActivity implements GlobalInterFace {
+public class InfoActivity extends AppCompatActivity implements GlobalInterFace, View.OnClickListener {
     private ListView infoLView;
+    private ImageView backImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +27,13 @@ public class InfoActivity extends AppCompatActivity implements GlobalInterFace {
     @Override
     public void findViewById() {
         infoLView=(ListView)findViewById(R.id.infoList);
+        backImg=(ImageView)findViewById(R.id.infoBack);
 
     }
 
     @Override
     public void setOnClickListener() {
+        backImg.setOnClickListener(this);
 
     }
 
@@ -43,4 +48,19 @@ public class InfoActivity extends AppCompatActivity implements GlobalInterFace {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.infoBack: {
+                backActivity();
+                break;
+            }
+        }
+
+    }
+
+    private void backActivity(){
+        finish();
+
+    }
 }
