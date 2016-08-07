be33ba3d805e6ce241c2d4616d4dd2939b960d5d
supriya <pandeydeepali0@gmail.com>
2016-08-07 09:22:44 +0530

    FoodApp Croma

--- app/src/main/java/com/croma/app/foodApp/Fooddetail.java
+++ app/src/main/java/com/croma/app/foodApp/Fooddetail.java
@@ -4,4 +4,14 @@ package com.croma.app.foodApp;
  * Created by suppi on 07/08/16.
  */
 public class Fooddetail {
+    public String foodName;
+    public String forwardImg;
+
+
+    Fooddetail(String foodName, String forwardImg){
+        this.foodName=foodName;
+        this.forwardImg=forwardImg;
+
+    }
+
 }
